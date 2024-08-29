dbaedef6b943fb7af664440da86885b50d01b0ef
Aleg <omarjandro00@gmail.com>
2024-08-29 10:57:19 -0600

    Add files via upload

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 Aug 2024 16:00:48 -0000
+vti_extenderversion:SR|12.0.0.6211
+vti_author:SR|EQ10\\Compu10
+vti_modifiedby:SR|EQ10\\Compu10
+vti_timecreated:TR|27 Aug 2020 12:06:44 -0000
+vti_nexttolasttimemodified:TR|27 Aug 2020 12:06:44 -0000
+vti_cacheddtm:TX|27 Aug 2020 12:06:44 -0000
+vti_filesize:IR|136799
+vti_backlinkinfo:VX|contact.html blog.html about.html furniture.html index.html
